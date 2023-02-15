aaae87064310d6d8154f155ea79778951cda16c9
otaviusedano <sedanootavio@gmail.com>
2023-02-14 21:46:22 -0300

    change: changes component header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import { IoIosPlanet, IoMdJournal, IoMdPerson } from "react-icons/io";
-import { ContainerLink } from "../Containers/Link";
+import { ContainerAnchor } from "../Containers/Anchor";
 import { ContainerLogo } from "../Containers/Logo";
+import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 
 import './styles.scss'
 
@@ -11,12 +12,38 @@ export function Header () {
         <IoIosPlanet color="#242424" fontSize="1.8rem"/>
         <h1>Otávio</h1>
       </ContainerLogo>
-      <ContainerLink>
-        <IoMdJournal color="#242424" fontSize="1.3rem"/> Projects
-      </ContainerLink>
-      <ContainerLink>
-        <IoMdPerson color="#242424" fontSize="1.3rem"/> Profile
-      </ContainerLink>
+      <ContainerAnchor>
+        <IoMdJournal color="#242424" fontSize="1.1rem"/>
+        <Link
+          to="projects"
+          spy={true}
+          smooth={true}
+          hashSpy={true}
+          offset={50}
+          duration={500}
+          delay={200}
+          isDynamic={true}
+          ignoreCancelEvents={false}
+        >
+          <span>Projects</span>
+        </Link>
+      </ContainerAnchor>
+      <ContainerAnchor>
+        <IoMdPerson color="#242424" fontSize="1.1rem"/>
+        <Link
+          to="about"
+          spy={true}
+          smooth={true}
+          hashSpy={true}
+          offset={50}
+          duration={500}
+          delay={200}
+          isDynamic={true}
+          ignoreCancelEvents={false}
+        >
+          <span>Profile</span>
+        </Link>
+      </ContainerAnchor>
     </header>
   )
 }
