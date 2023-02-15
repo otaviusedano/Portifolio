2ee19a230d8ff5ee88dc6920f56e0cddf802b6b3
otaviusedano <sedanootavio@gmail.com>
2023-02-14 21:48:58 -0300

    feature: add new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { IoIosPlanet, IoIosSunny, IoIosRocket, IoIosMoon} from "react-icons/io";
 
 import { ContainerSection } from './components/Containers/Section'
@@ -6,21 +5,27 @@ import { ContainerText } from './components/Containers/Text'
 import { Header } from './components/Header'
 import { SubTitle } from './components/SubTitle'
 import { Title } from './components/Title'
+import { ContainerLinks } from './components/Containers/Links';
+import { Link } from './components/Link';
+import { ContainerOrbit } from './components/Containers/Orbit';
+import { ContainerProjects } from './components/Containers/Projects';
+import { Project } from './components/Project';
+import { Link as LinkScroll, Button, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
+import projeto1 from './assets/project 1.png'
 
 import './App.scss'
-import { ContainerOrbit } from './components/Containers/Orbit';
+import { Footer } from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Header />
-      <ContainerSection>
-        <ContainerOrbit planet='saturn'>
-          <IoIosPlanet color='#242424' fontSize={120} />
-        </ContainerOrbit>
-        <ContainerOrbit planet='uranus'>
+      <ContainerOrbit planet='saturn'>
+        <IoIosPlanet color='#242424' fontSize={120} />
+      </ContainerOrbit>
+      <ContainerSection sectionName='main' id='main'>
+        {/* <ContainerOrbit planet='uranus'>
           <IoIosPlanet color='blue' fontSize={80} />
         </ContainerOrbit>
         <ContainerOrbit planet='sun'>
@@ -31,13 +36,60 @@ function App() {
         </ContainerOrbit>
         <ContainerOrbit planet='moon'>
           <IoIosMoon color='#242424' fontSize={80} />
-        </ContainerOrbit>
+        </ContainerOrbit> */}
         <ContainerText>
-          <SubTitle>Otávio Sedano</SubTitle>
+          <SubTitle>OTAVIO SEDANO</SubTitle>
           <Title>Desenvolvedor Web</Title>
           <SubTitle>Lorem ipsum dolor sit amet consectetur. amet consectetur amet consectetur.</SubTitle>
+          <ContainerLinks>
+            <LinkScroll
+              to="about"
+              spy={true}
+              smooth={true}
+              hashSpy={true}
+              offset={50}
+              duration={500}
+              delay={300}
+              isDynamic={true}
+              ignoreCancelEvents={false}
+            >
+              <Link>
+                Sobre mim
+              </Link>
+            </LinkScroll>
+                      ou
+            <LinkScroll
+              to="projects"
+              spy={true}
+              smooth={true}
+              hashSpy={true}
+              offset={50}
+              duration={500}
+              delay={300}
+              isDynamic={true}
+              ignoreCancelEvents={false}
+            >
+              <Link>
+                Projetos
+              </Link>
+            </LinkScroll>
+          </ContainerLinks>
         </ContainerText>
       </ContainerSection>
+      <ContainerSection sectionName='projects' id='projects'>
+        <ContainerText>
+          <SubTitle>SELECIONADOS</SubTitle>
+          <Title>Projetos</Title>
+        </ContainerText>
+        <ContainerProjects>
+          <Project img={projeto1} link='https://translation-rouge.vercel.app/' projectName='Translation' bodyText='Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ut animi commodi.' />
+          <Project img={projeto1} link='https://translation-rouge.vercel.app/' projectName='Translation' bodyText='Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ut animi commodi.' />
+          <Project img={projeto1} link='https://translation-rouge.vercel.app/' />
+        </ContainerProjects>
+      </ContainerSection>
+      <ContainerSection sectionName='about' id='about'>
+      </ContainerSection>
+      <Footer></Footer>
     </>
   )
 }
